5a662efae6272c5f6bc94660bbdf54461a73301f
nurlan.kamenov <knurlannit@gmail.com>
2022-08-12 17:59:13 +0600

    fix empty word. realized word count function

--- countutil/wordcounter.go
+++ countutil/wordcounter.go
@@ -3,6 +3,9 @@ package countutil
 import "strings"
 
 func WordCounter(in string) int {
+	if len(strings.TrimSpace(in)) < 1 {
+		return 0
+	}
 	s := strings.Split(strings.TrimSpace(in), " ")
 	return len(s)
 }
